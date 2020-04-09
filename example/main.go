454874e820e263da5a90cac1acbf3d81ea37e092
kunnpuu <727759145@qq.com>
2020-04-09 21:22:04 +0800

    version 0.1

--- example/main.go
+++ example/main.go
@@ -1,143 +1,17 @@
-// Copyright 2019 Andy Pan. All rights reserved.
-// Copyright 2017 Joshua J Baker. All rights reserved.
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"flag"
 	"fmt"
-	"log"
 	"net/http"
-	"strconv"
-	"time"
-	"unsafe"
-
-	"github.com/panjf2000/gnet"
+	"github.com/kunnpuu/ghttp"
 )
 
-var res string
-
-type httpServer struct {
-	*gnet.EventServer
-}
-
-var errMsg = "Internal Server Error"
-var errMsgBytes = []byte(errMsg)
-
-type httpCodec struct {
-	req http.Request
-}
-
-func (hc *httpCodec) Encode(c gnet.Conn, buf []byte) (out []byte, err error) {
-	if c.Context() == nil {
-		return buf, nil
-	}
-	resp := NewGResponse()
-	resp.WriteHeader(500)
-	return appendResp(out, "500 Error", "", errMsg+"\n"), nil
-}
-
-func (hc *httpCodec) Decode(c gnet.Conn) (out []byte, err error) {
-	buf := c.Read()
-	c.ResetBuffer()
-
-	// process the pipeline
-	var leftover []byte
-pipeline:
-	leftover, err = parseReq(buf, &hc.req)
-	// bad thing happened
-	if err != nil {
-		c.SetContext(err)
-		return nil, err
-	} else if len(leftover) == len(buf) {
-		// request not ready, yet
-		return
-	}
-	out = appendHandle(out, res)
-	buf = leftover
-	goto pipeline
-}
-
-func (hs *httpServer) OnInitComplete(srv gnet.Server) (action gnet.Action) {
-	log.Printf("HTTP server is listening on %s (multi-cores: %t, loops: %d)\n",
-		srv.Addr.String(), srv.Multicore, srv.NumEventLoop)
-	return
-}
-
-func (hs *httpServer) React(frame []byte, c gnet.Conn) (out []byte, action gnet.Action) {
-	if c.Context() != nil {
-		// bad thing happened
-		out = errMsgBytes
-		action = gnet.Close
-		return
-	}
-	// handle the request
-	out = frame
-	return
-}
-
 func main() {
-	var port int
-	var multicore bool
-
-	// Example command: go run http.go --port 8080 --multicore=true
-	flag.IntVar(&port, "port", 8080, "server port")
-	flag.BoolVar(&multicore, "multicore", true, "multicore")
-	flag.Parse()
-
-	res = "Hello World!\r\n"
-
-	http := new(httpServer)
-	hc := new(httpCodec)
-
-	// Start serving!
-	log.Fatal(gnet.Serve(http, fmt.Sprintf("tcp://:%d", port), gnet.WithMulticore(multicore), gnet.WithCodec(hc)))
-}
-
-// appendHandle handles the incoming request and appends the response to
-// the provided bytes, which is then returned to the caller.
-func appendHandle(b []byte, res string) []byte {
-	return appendResp(b, "200 OK", "", res)
-}
-
-// appendResp will append a valid http response to the provide bytes.
-// The status param should be the code plus text such as "200 OK".
-// The head parameter should be a series of lines ending with "\r\n" or empty.
-func appendResp(b []byte, status, head, body string) []byte {
-	b = append(b, "HTTP/1.1"...)
-	b = append(b, ' ')
-	b = append(b, status...)
-	b = append(b, '\r', '\n')
-	b = append(b, "Server: gnet\r\n"...)
-	b = append(b, "Date: "...)
-	b = time.Now().AppendFormat(b, "Mon, 02 Jan 2006 15:04:05 GMT")
-	b = append(b, '\r', '\n')
-	if len(body) > 0 {
-		b = append(b, "Content-Length: "...)
-		b = strconv.AppendInt(b, int64(len(body)), 10)
-		b = append(b, '\r', '\n')
-	}
-	b = append(b, head...)
-	b = append(b, '\r', '\n')
-	if len(body) > 0 {
-		b = append(b, body...)
-	}
-	return b
-}
-
-func b2s(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
-}
-
-// parseReq is a very simple http request parser. This operation
-// waits for the entire payload to be buffered before returning a
-// valid request.
-func parseReq(data []byte, req *http.Request) (leftover []byte, err error) {
-	req, err = http.ReadRequest(bufio.NewReader(bytes.NewBuffer(data)))
-	// not enough data
-	return nil, nil
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "hello http server")
+	})
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "pong")
+	})
+	ghttp.ListenAndServe(":8080", nil)
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/kunnpuu/ghttp
 
 go 1.13
 
-require (
-	github.com/panjf2000/gnet v1.1.1
-	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
-)
+require github.com/panjf2000/gnet v1.1.1
